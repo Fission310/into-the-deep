5da2c1dd3af36a51fadb7f3d0a55a71909b2c928
Turo's Zenbook <tgturo4@gmail.com>
2024-09-11 17:16:45 -0400

    made drivetrain mechanism

---
+++ hardware/mechanisms/Drivetrain.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.hardware.mechanisms;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.stuyfission.fissionlib.util.Mechanism;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Config
+public class Drivetrain extends Mechanism {
+
+    private SampleMecanumDrive drivetrain;
+
+    public Drivetrain(LinearOpMode opMode) {
+        this.opMode = opMode;
+    }
+
+    @Override
+    public void init(HardwareMap hwMap) {
+        drivetrain = new SampleMecanumDrive(hwMap);
+        drivetrain.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    @Override
+    public void loop(Gamepad gamepad) {
+        drivetrain.setWeightedDrivePower(
+                new Pose2d(
+                        -gamepad.left_stick_y,
+                        -gamepad.left_stick_x,
+                        -gamepad.right_stick_x
+                )
+        );
+
+        drivetrain.update();
+
+    }
+
+}
