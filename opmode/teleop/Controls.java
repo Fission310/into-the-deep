09a34f85728a4e28ae6480f94ff46566b6ddcd67
Turo's Zenbook <tgturo4@gmail.com>
2024-10-23 16:26:09 -0400

    forgot to commit Controls lol

--- opmode/teleop/Controls.java
+++ opmode/teleop/Controls.java
@@ -2,14 +2,9 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 import com.stuyfission.fissionlib.input.GamepadStatic.Input;
 
 public class Controls {
-
-    public static final Input SCORE = Input.RIGHT_BUMPER;
-    public static final Input GRAB = Input.A;
     public static final Input INTAKE = Input.LEFT_TRIGGER;
     public static final Input OUTTAKE = Input.RIGHT_TRIGGER;
     public static final Input MIDDLE_WRIST = Input.A;
-    public static final Input LEFT_WRIST = Input.X;
-    public static final Input RIGHT_WRIST = Input.Y;
     public static final Input LOW = Input.DPAD_DOWN;
     public static final Input MEDIUM = Input.DPAD_LEFT;
     public static final Input HIGH = Input.DPAD_UP;
