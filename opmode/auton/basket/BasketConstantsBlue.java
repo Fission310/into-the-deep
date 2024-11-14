7315e79a12edf4ecd2a8b6a10e63bddf0ac3edee
Vlam60 <vincentl170@nycstudents.net>
2024-11-14 14:45:09 -0500

    bluebasketconstants

--- opmode/auton/basket/BasketConstantsBlue.java
+++ opmode/auton/basket/BasketConstantsBlue.java
@@ -2,11 +2,45 @@ package org.firstinspires.ftc.teamcode.opmode.auton.basket;
 
 import static org.firstinspires.ftc.teamcode.opmode.auton.util.GameConstants.TILE_LENGTH;
 
+import android.service.quicksettings.Tile;
+
 import org.firstinspires.ftc.teamcode.opmode.auton.basket.setup.BasketConstants;
 import org.firstinspires.ftc.teamcode.opmode.auton.util.Constant;
 
 public class BasketConstantsBlue extends BasketConstants {
     public BasketConstantsBlue() {
+
+        CHAMBER = new Constant(
+            TILE_LENGTH * 0.75 , 0, Math.toRadians(180)
+        );
+
+        WALL_SAMPLE = new Constant(
+            TILE_LENGTH * 3, TILE_LENGTH, Math.toRadians(0)
+        );
+
+        CENTER_SAMPLE = new Constant(
+            TILE_LENGTH * 2.5, TILE_LENGTH, Math.toRadians(0)
+        );
+
+        FAR_SAMPLE = new Constant(
+            TILE_LENGTH * 2, TILE_LENGTH, Math.toRadians(0)
+        );
+
+        BASKET_1 = new Constant(
+        TILE_LENGTH*2.5, TILE_LENGTH*2.5, Math.toRadians(225)
+        );
+
+        BASKET_2 = new Constant(
+        TILE_LENGTH*2.5, TILE_LENGTH*2.5, Math.toRadians(225)
+        );
+
+        BASKET_3 = new Constant(
+        TILE_LENGTH*2.5, TILE_LENGTH*2.5, Math.toRadians(225)
+        );
+
+        PARK = new Constant(
+            0, TILE_LENGTH*3, Math.toRadians(0)
+        );
 
     }
 }
--- opmode/auton/basket/BasketConstantsRed.java
+++ opmode/auton/basket/BasketConstantsRed.java
@@ -1,9 +1,42 @@
 package org.firstinspires.ftc.teamcode.opmode.auton.basket;
 
+import static org.firstinspires.ftc.teamcode.opmode.auton.util.GameConstants.TILE_LENGTH;
+
 import org.firstinspires.ftc.teamcode.opmode.auton.basket.setup.BasketConstants;
+import org.firstinspires.ftc.teamcode.opmode.auton.util.Constant;
 
 public class BasketConstantsRed extends BasketConstants {
     public BasketConstantsRed() {
+        CHAMBER = new Constant(
+            TILE_LENGTH, 0, Math.toRadians(0)
+        );
 
+        WALL_SAMPLE = new Constant(
+            TILE_LENGTH, -TILE_LENGTH*3, Math.toRadians(180)
+        );
+
+        CENTER_SAMPLE = new Constant(
+            TILE_LENGTH, -TILE_LENGTH*2.5, Math.toRadians(180)
+        );
+
+        FAR_SAMPLE = new Constant(
+            TILE_LENGTH, -TILE_LENGTH*2, Math.toRadians(180)
+        );
+
+        BASKET_1 = new Constant(
+        TILE_LENGTH*3, -TILE_LENGTH*3, Math.toRadians(315)
+        );
+
+        BASKET_2 = new Constant(
+        TILE_LENGTH*3, -TILE_LENGTH*3, Math.toRadians(315)
+        );
+
+        BASKET_3 = new Constant(
+        TILE_LENGTH*3, -TILE_LENGTH*3, Math.toRadians(315)
+        );
+
+        PARK = new Constant(
+        TILE_LENGTH*3, 0, Math.toRadians(180)
+        );
     }
 }
