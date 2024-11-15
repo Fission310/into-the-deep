a1f7246a4559d0c00a7bf6b25a80e256737af0ca
Vlam60 <vincentl170@nycstudents.net>
2024-11-15 18:25:24 -0500

    did main.java

--- hardware/mechanisms/Scoring.java
+++ hardware/mechanisms/Scoring.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.hardware.mechanisms;
 
-public class Scoring {
+import java.lang.Thread.State;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.opmode.teleop.Controls;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.stuyfission.fissionlib.command.Command;
+import com.stuyfission.fissionlib.command.CommandSequence;
+import com.stuyfission.fissionlib.input.GamepadStatic;
+import com.stuyfission.fissionlib.util.Mechanism;
+
+public class Scoring extends Mechanism{
+    public Scoring(LinearOpMode opMode) {
+        this.opMode = opMode;
+    }
+
+    @Override
+    public void init(HardwareMap hwMap) {
+
+    }
 }
