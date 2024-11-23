672055fb0b619cc637b4507b2f008a9ca5a87a45
Vlam60 <vincentl170@nycstudents.net>
2024-11-23 17:44:50 -0500

    did we the auton commanded

--- opmode/auton/basket/setup/BasketAuto.java
+++ opmode/auton/basket/setup/BasketAuto.java
@@ -15,13 +15,17 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.stuyfission.fissionlib.command.AutoCommandMachine;
+import com.stuyfission.fissionlib.command.Command;
+import com.stuyfission.fissionlib.command.CommandSequence;
 
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 public class BasketAuto extends LinearOpMode{
-    boolean reflect;
-    Color color;
-    BasketConstants basketConstants;
+    private boolean reflect;
+    private boolean busy = false;
+    private Color color;
+    private BasketConstants basketConstants;
     public BasketAuto(Color color, BasketConstants basketConstants){
         this.color = color;
         this.basketConstants = basketConstants;
@@ -40,6 +44,48 @@ public class BasketAuto extends LinearOpMode{
     private Scoring scoring;
     private Telescope telescope;
     private Wrist wrist;
+
+    private Command intakeStartCommand = () -> intake.intake();
+    private Command intakeStopCommand = () -> intake.stop();
+
+    private Command chamberCommand = () -> drive.followTrajectorySequenceAsync(chamberTraj);
+    private Command farSampleCommand = () -> drive.followTrajectorySequenceAsync(farTraj);
+    private Command basket1Command = () -> drive.followTrajectorySequenceAsync(basket1Traj);
+    private Command centerSampleCommand = () -> drive.followTrajectorySequenceAsync(centerTraj);
+    private Command basket2Command = () -> drive.followTrajectorySequenceAsync(basket2Traj);
+    private Command wallSampleCommand = () -> drive.followTrajectorySequenceAsync(wallTraj);
+    private Command basket3Command = () -> drive.followTrajectorySequenceAsync(basket3Traj);
+
+    private CommandSequence chamberSequence = new CommandSequence()
+            .addCommand(chamberCommand)
+            .build();
+    private CommandSequence farSampleSequence = new CommandSequence()
+            .addCommand(farSampleCommand)
+            .build();
+    private CommandSequence basket1Sequence = new CommandSequence()
+            .addCommand(basket1Command)
+            .build();
+    private CommandSequence centerSampleSequence = new CommandSequence()
+            .addCommand(centerSampleCommand)
+            .build();
+    private CommandSequence basket2Sequence = new CommandSequence()
+            .addCommand(basket2Command)
+            .build();
+    private CommandSequence wallSampleSequence = new CommandSequence()
+            .addCommand(wallSampleCommand)
+            .build();
+    private CommandSequence basket3Sequence = new CommandSequence()
+            .addCommand(basket3Command)
+            .build();
+    private AutoCommandMachine commandMachine = new AutoCommandMachine()
+            .addCommandSequence(chamberSequence)
+            .addCommandSequence(farSampleSequence)
+            .addCommandSequence(basket1Sequence)
+            .addCommandSequence(centerSampleSequence)
+            .addCommandSequence(basket2Sequence)
+            .addCommandSequence(wallSampleSequence)
+            .addCommandSequence(basket3Sequence)
+            .build();
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -100,12 +146,12 @@ public class BasketAuto extends LinearOpMode{
                 .build();
         waitForStart();
 
-//        while (opModeIsActive() && !isStopRequested() && !commandMachine.hasCompleted()) {
-//            drive.update();
+        while (opModeIsActive() && !isStopRequested() && !commandMachine.hasCompleted()) {
+            drive.update();
 //            slides.update();
-//            commandMachine.run(drive.isBusy() || busy);
-//            telemetry.update();
-//        }
+            commandMachine.run(drive.isBusy() || busy);
+            telemetry.update();
+        }
     }
     public Pose2d reflectX(Pose2d pose) {
         if (reflect) {
