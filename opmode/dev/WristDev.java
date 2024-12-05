3d2c213d020f67e99868ef594c615af8b1766173
ONLYIWIN21 <flintthecuber@gmail.com>
2024-12-04 20:30:33 -0500

    teleop works!

--- opmode/dev/WristDev.java
+++ opmode/dev/WristDev.java
@@ -1,16 +1,21 @@
 package org.firstinspires.ftc.teamcode.opmode.dev;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.stuyfission.fissionlib.input.GamepadStatic;
+import com.stuyfission.fissionlib.input.GamepadStatic.Input;
 
 import org.firstinspires.ftc.teamcode.hardware.mechanisms.Wrist;
 import org.firstinspires.ftc.teamcode.opmode.teleop.Controls;
 
+@Config
 @TeleOp(name = "Wrist Dev", group = "dev")
 public class WristDev extends LinearOpMode {
 
     private Wrist wrist = new Wrist(this);
+    public static double RIGHT_POS = 0.7;
+    public static double LEFT_POS = 0.7;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -28,6 +33,8 @@ public class WristDev extends LinearOpMode {
                 wrist.clipPos();
             } else if (GamepadStatic.isButtonPressed(gamepad1, Controls.PIVOT_BASKET)) {
                 wrist.basketPos();
+            } else if (GamepadStatic.isButtonPressed(gamepad1, Input.DPAD_UP)) {
+                wrist.updatePos(LEFT_POS, RIGHT_POS);
             }
         }
     }
