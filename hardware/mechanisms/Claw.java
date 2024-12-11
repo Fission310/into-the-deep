cea982ecf15c31827098b248e9a411c0cd0d3567
ONLYIWIN21 <flintthecuber@gmail.com>
2024-12-11 15:49:02 -0500

    intake

--- hardware/mechanisms/Claw.java
+++ hardware/mechanisms/Claw.java
@@ -2,9 +2,10 @@ package org.firstinspires.ftc.teamcode.hardware.mechanisms;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction;
 import com.stuyfission.fissionlib.input.GamepadStatic;
 import com.stuyfission.fissionlib.util.Mechanism;
 
@@ -14,24 +15,27 @@ import org.firstinspires.ftc.teamcode.opmode.teleop.Controls;
 public class Claw extends Mechanism {
     public static double GRAB_POS = 0.275;
     public static double RELEASE_POS = 0.14;
+    public static double GRAB_POWER = 1;
+    public static double RELEASE_POWER = -1;
 
-    private Servo clawServo;
+    private CRServo clawServo;
 
     public Claw(LinearOpMode opMode) {
         this.opMode = opMode;
     }
 
     public void grab() {
-        clawServo.setPosition(GRAB_POS);
+        clawServo.setPower(GRAB_POWER);
     }
 
     public void release() {
-        clawServo.setPosition(RELEASE_POS);
+        clawServo.setPower(RELEASE_POWER);
     }
 
     @Override
     public void init(HardwareMap hwMap) {
-        clawServo = hwMap.get(Servo.class, "clawServo");
+        clawServo = hwMap.get(CRServo.class, "clawServo");
+        clawServo.setDirection(Direction.REVERSE);
         grab();
     }
 
