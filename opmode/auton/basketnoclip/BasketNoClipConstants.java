0de5a20d475caa5b4fdc34329b2d20577d224827
Daniel Park <dpark070111@gmail.com>
2025-01-31 19:11:48 -0500

    inital auto

--- opmode/auton/basketnoclip/BasketNoClipConstants.java
+++ opmode/auton/basketnoclip/BasketNoClipConstants.java
@@ -9,7 +9,7 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class BasketNoClipConstants {
     public static Constant START = new Constant(
-            -TILE_LENGTH - BOT_WIDTH / 2,
+            -TILE_LENGTH * 2 + BOT_WIDTH / 2,
             -WALL_POS + BOT_LENGTH / 2,
             UP
     );
@@ -19,19 +19,19 @@ public class BasketNoClipConstants {
             Math.toRadians(43)
     );
     public static Constant BASKET_2 = new Constant(
-            -TILE_LENGTH * 2.5 - BOT_WIDTH + 23.5,
-            -TILE_LENGTH * 5 / 2 + BOT_LENGTH / 2 - 0.3,
-            Math.toRadians(35)
-    );
-    public static Constant BASKET_3 = new Constant(
             -TILE_LENGTH * 2.5 - BOT_WIDTH + 20.5,
             -TILE_LENGTH * 5 / 2 + BOT_LENGTH / 2 - 2,
-            Math.toRadians(38)
+            Math.toRadians(43)
+    );
+    public static Constant BASKET_3 = new Constant(
+            -TILE_LENGTH * 2.5 - BOT_WIDTH + 19.5,
+            -TILE_LENGTH * 5 / 2 + BOT_LENGTH / 2 - 3,
+            Math.toRadians(43)
     );
     public static Constant BASKET_4 = new Constant(
             -TILE_LENGTH * 2.5 - BOT_WIDTH + 22.5,
             -TILE_LENGTH * 5 / 2 + BOT_LENGTH / 2 - 1,
-            Math.toRadians(40)
+            Math.toRadians(30)
     );
     public static Constant FAR_SAMPLE = new Constant(
             -TILE_LENGTH * 2.5 - BOT_WIDTH + 26,
@@ -54,8 +54,13 @@ public class BasketNoClipConstants {
             UP + Math.toRadians(7)
     );
     public static Constant WALL_SAMPLE = new Constant(
-            -TILE_LENGTH - BOT_WIDTH / 2 - 10,
-            -TILE_LENGTH  - BOT_LENGTH / 2 + 5.3,
-            UP + Math.toRadians(90)
+            -TILE_LENGTH * 2.5 - BOT_WIDTH + 10.0,
+            -TILE_LENGTH * 5 / 2 + BOT_LENGTH / 2 - 3.5,
+            UP - Math.toRadians(5)
+    );
+    public static Constant WALL_SAMPLE_INT = new Constant(
+            -TILE_LENGTH * 2.5 - BOT_WIDTH + 12.0,
+            -TILE_LENGTH * 5 / 2 + BOT_LENGTH / 2 - 1,
+            UP + Math.toRadians(25)
     );
 }
