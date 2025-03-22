e8fce65dcc085ccabb858379ca331253984e0904
ghurwitz1 <ghurwitz1@gmail.com>
2025-03-21 20:00:26 -0400

    changed wrist pos

--- hardware/mechanisms/Wrist.java
+++ hardware/mechanisms/Wrist.java
@@ -23,7 +23,7 @@ public class Wrist extends Mechanism {
     public static double[][] AUTO_INTAKE_LL_POS =  { { 0.48, 0.48 }, { 0.59, 0.58 }, { 0.59, 0.58 }, { 0.59, 0.58 } };
     public static double[][] AUTO_BASKET_POS =  { { 0.315, 0.315 }, { 0.59, 0.58 }, { 0.59, 0.58 }, { 0.59, 0.58 } };
     public static double[][] AUTO_MID_POS =  { { 0.460, 0.460 }, { 0.59, 0.58 }, { 0.59, 0.58 }, { 0.59, 0.58 } };
-    public static double[][] INTAKE_POS = { { 0.47, 0.505 }, { 0.59, 0.58 }, { 0.59, 0.58 }, { 0.59, 0.58 } };
+    public static double[][] INTAKE_POS = { { 0.48, 0.505 }, { 0.59, 0.58 }, { 0.59, 0.58 }, { 0.59, 0.58 } };
     public static double[][] INTAKE_MID_POS = { { 0.330, 0.330 }, { 0.59, 0.58 }, { 0.59, 0.58 }, { 0.59, 0.58 } };
     public static double[][] INTAKE_SHORT_POS = { { 0.460, 0.460 }, { 0.59, 0.58 }, { 0.59, 0.58 }, { 0.59, 0.58 } };
     public static double[][] INTAKE_DOWN_POS = { { 0.740, 0.510 }, { 0.76, 0.53 }, { 0.76, 0.53 }, { 0.76, 0.53 } };
