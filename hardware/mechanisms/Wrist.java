c89b6d4c69a71435231f67af9bda947f888c350f
XY <xuanyao0923@gmail.com>
2025-04-27 01:50:04 -0400

    tuning

--- hardware/mechanisms/Wrist.java
+++ hardware/mechanisms/Wrist.java
@@ -18,8 +18,8 @@ public class Wrist extends Mechanism {
     private Servo wristServoLeft;
     private Servo wristServoAdjuster;
 
-    public static double INTAKE_DOWN_ABIT = 0.006;
-    public static double BASKET_DOWN_ABIT = -0.15;
+    public static double INTAKE_DOWN_ABIT = 0.003;
+    public static double BASKET_DOWN_ABIT = -0.01;
     public static double INTAKE_FLICKABIT = 0.4;
     public static double[][] INTAKE_LEFT_POS = {{.68,.12},{.8,.8},{.8,.8},{.8,.8}};
     public static double[][] AUTO_INTAKE_POS =  { { 0.64, 0.64 }, { 0.59, 0.58 }, { 0.59, 0.58 }, { 0.59, 0.58 } };
@@ -30,9 +30,9 @@ public class Wrist extends Mechanism {
     public static double[][] INTAKE_MID_POS = { { .6875, 0.330 }, { 0.59, 0.58 }, { 0.59, 0.58 }, { 0.59, 0.58 } };
     public static double[][] INTAKE_SHORT_POS = { { .65, 0.65 }, { 0.59, 0.58 }, { 0.59, 0.58 }, { 0.59, 0.58 } };
     public static double[][] INTAKE_DOWN_POS = { { .8, .19}, { 0.76, 0.53 }, { 0.76, 0.53 }, { 0.76, 0.53 } };
-    public static double[][] FRONT_POS = { { .5, 0.25 }, { 0.25, 0.25 }, { 0.25, 0.25 }, { 0.25, 0.25 } };
+    public static double[][] FRONT_POS = { { .74, 0.15 }, { 0.25, 0.25 }, { 0.25, 0.25 }, { 0.25, 0.25 } };
     public static double[][] WALL_POS = { { 0.325, 0.315 }, { 0.43, 0.42 }, { 0.43, 0.42 }, { 0.43, 0.42 } };
-    public static double[][] BASKET_POS = { {.19, .54 }, { 0.35, 0.35 }, { 0.35, 0.35 }, { 0.35, 0.35 } };
+    public static double[][] BASKET_POS = { {.41, .5 }, { 0.35, 0.35 }, { 0.35, 0.35 }, { 0.35, 0.35 } };
     public static double[][] CLIP_POS = { { 0.545, 0.545 }, { 0.65, 0.65 }, { 0.65, 0.65 }, { 0.65, 0.65 } };
     public static double[][] CLIP_SCORE_POS = { { 0.5, 0.53 }, { 0.65, 0.65 }, { 0.65, 0.65 }, { 0.65, 0.65 } };
     public static double[][] BACK_POS = { { 0.15, 0.15 }, { 0.25, 0.25 }, { 0.25, 0.25 }, { 0.25, 0.25 } };
@@ -63,7 +63,7 @@ public class Wrist extends Mechanism {
     public void leftReverse() {wristServoLeft.setDirection(Direction.REVERSE);}
 
     public void intakeABit(){
-        wristServoRight.setPosition(currPos[wristPos][0] + INTAKE_DOWN_ABIT);
+        wristServoRight.setPosition(currPos[wristPos][0] + INTAKE_DOWN_ABIT );
         wristServoLeft.setPosition(currPos[wristPos][1] + INTAKE_DOWN_ABIT);
     }
 
